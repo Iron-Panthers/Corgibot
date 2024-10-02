309f12367696df333409e98c8e9ba10202037ebd
pevocarevantaranov <sdozorov2008@gmail.com>
2024-10-01 17:58:46 -0700

    Update version
    updated from 11 to 17

--- frc2022-mecanum-drive-ex/src/main/java/frc/robot/Constants.java
+++ frc2022-mecanum-drive-ex/src/main/java/frc/robot/Constants.java
@@ -1,7 +1,12 @@
 package frc.robot;
 
-public class Constants {
-    public class Drive {
+import java.util.List;
+
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+public final class Constants {
+    public final class Drive {
         public static final double m_deadband = 0.1;
          public class MotorPorts {
             public static final int kFrontLeftId = 0x03;
@@ -12,5 +17,56 @@ public class Constants {
 
 
     }
+
+    
+ 
+  
+  public static final class Vision {
+    
+    public static record VisionSource(String name, Transform3d robotToCamera) {}
+
+    public static final List<VisionSource> VISION_SOURCES =
+        List.of(
+            /*new VisionSource(
+                "frontCam",
+                new Transform3d(
+                    new Translation3d(
+                        -0.305, // front/back
+                        -0.2286, // left/right
+                        -0.2159 // up/down
+                        ),
+                    new Rotation3d(
+                        0,
+                        Math.toRadians(30), // angle up/down
+                        Math.toRadians(180)))),
+            new VisionSource(
+                "backCam",
+                new Transform3d(
+                    new Translation3d(
+                        -0.2796, // front/back
+                        -0.2286, // left/right
+                        -0.2159 // up/down
+                        ),
+                    new Rotation3d(
+                        0,
+                        Math.toRadians(30), // angle up/down
+                        Math.toRadians(180)))),*/
+            new VisionSource(
+                "backUpCam",
+                new Transform3d(
+                    new Translation3d(
+                        -0.2796, // front/back
+                        0.2286, // left/right
+                        -0.2159 // up/down
+                        ),
+                    new Rotation3d(
+                        0,
+                        Math.toRadians(30), // angle up/down
+                        Math.toRadians(180)))));
+
+    public static final int THREAD_SLEEP_DURATION_MS = 5;
+  }
+
+
     
 }
--- frc2022-mecanum-drive-ex/src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ frc2022-mecanum-drive-ex/src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,10 +1,31 @@
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+package frc.robot.subsystems;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.MatBuilder;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+import frc.robot.Constants.Vision;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.targeting.PhotonPipelineResult;
 
-public class VisionSubsystem extends SubsystemBase {
-    private PhotonCamera camera;
-
-    public VisionSubsystem() {
-        camera = new PhotonCamera("limelight");
-    }
-}
+public class VisionSubsystem {}
